e39d54fe7c18418c908b96f9974bce8bcaf75ffc
Arshad Salim <arshadsalim00@gmail.com>
2021-05-12 14:40:48 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+require("./src/handlers/error-handler");
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const db = require("./src/models");
+const {productRouter, userRouter} = require("./src/routes");
+const authMiddleware = require("./src/middlewares/auth-middleware");
+const PORT_NUMBER = 3000;
+
+const ecomm = express();
+
+ecomm.use(cors());
+ecomm.use(helmet());
+ecomm.use(morgan("dev"));
+ecomm.use(authMiddleware);
+ecomm.use(express.json());
+ecomm.use(express.urlencoded({ extended:true }));
+
+db.sequelize.sync();
+
+ecomm.use("/products", productRouter);
+ecomm.use("/auth", userRouter)
+
+ecomm.listen(PORT_NUMBER, (error) => {
+    if(error){
+        throw error;
+    } else {
+        console.log(`Server has started running at http://localhost:${PORT_NUMBER}`);
+    }
+});
---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    SECRET_KEY:
+        "tTb52pzNet8vCe2vGhWrrY-UeRRf4GvqmZlBZU72N7ZW4uMssVLf1CwgezYhvHKHlVvwnIHgx1lXNa8WSBA02NsICUzbnuHcEvSi6J_nduJylkprj7DxJP7OcD0PPaz5OGEFLve8wUmQI5lycLndw662mLTBSrBgSP_pBKi4P4E"
+};
